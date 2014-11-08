aa60a2000c11080c37229d922fa1f70cc7ed75bf
Siddharth <sids.aquarius@gmail.com>
2014-11-08 10:33:20 -0200

    Add google analytics tracking

---
+++ js/analytics.js
@@ -0,0 +1,1 @@
+!function(e,t,a,n,r){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var g=t.getElementsByTagName(a)[0],m=t.createElement(a),s="dataLayer"!=n?"&l="+n:"";m.async=!0,m.src="//www.googletagmanager.com/gtm.js?id="+r+s,g.parentNode.insertBefore(m,g)}(window,document,"script","dataLayer","GTM-N2HFT7");
