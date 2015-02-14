17d695d211dcc7a7d9fea6c51c38552a2680c36a
Henrique Sa <henriquepss@gmail.com>
2015-02-13 19:35:50 -0600

    Moved style snippet

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+(function($){
+
+	var jWindow = $(window);
+
+	Docbase.run({
+	  method: 'github',
+	  path: 'src',
+	  map: {
+	      file: 'map.json',
+	      path: '/'
+	  },
+	  github: {
+	    user: 'appbaseio',
+	    repo: 'Docs',
+	    path: 'src',
+	    branch: 'master'
+	  },
+	  file: {
+	      src: 'map.json',
+	      path: 'src'
+	  },
+	  indexType: 'markdown',
+	  indexSrc: 'v2.0/javascript/quick-start',
+	  editGithubBtn: true,
+	  html5mode: false,
+	  flatdocHtml: 'html/flatdoc.html',
+	  angularAppName: 'docbaseApp'
+
+	});
+
+	jWindow.on('docbase:ready', function(){
+		$("blockquote>pre:has(code.lang-curl)").each(function() {
+		  $(this).parent().css({"float": "left",
+		                         "width": "510px",
+		                         "margin-left": "20px",
+		                         "margin-right": "20px",
+		                         "color": "#333"});
+		  $(this).css({"padding": "10px"});
+		});
+	});
+
+})(window.jQuery);
