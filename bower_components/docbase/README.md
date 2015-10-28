fa4b3d8cdcd6c515ce58b7ab6282bfee2b9bd5d2
Siddharth Kothari <sids.aquarius@gmail.com>
2015-10-28 16:08:01 -0500

    update bower dependencies

--- bower_components/docbase/README.md
+++ bower_components/docbase/README.md
@@ -1,6 +1,9 @@
-# DocBase
-A fast and flexible single-page documentation engine.
-With your existing (or new) markdown files, organize them in a simpe version/category folder structure and have dynamically generated menus and links.
+# Docbase
+A fast and flexible single-page documentation engine. Create documentation hubs with versioning and beautifully rendered menus; works with your existing (or new) Github hosted markdown files.
+
+### Used by:
+
+[![](http://app.zeedhi.com/teknisa/docs/images/log.png)](http://app.zeedhi.com/teknisa/docs/#/) [![](http://i.imgur.com/0MwyOZL.png?1)](http://docs.appbase.io)
 
 ## Installation
 
@@ -8,18 +11,84 @@ With your existing (or new) markdown files, organize them in a simpe version/cat
 
 ## Usage
 
-Simply change the parameters to your project's spec on the index.html file. The map.json structure is the following:
+### Step 1: Configuring Index.html
 
-    { "version_name": [
+Copy the index.html file from docbase's root folder. You can change the parameters for your project's spec in this file.
+
+```js
+index.html
+
+.
+.
+.
+  <script type="text/javascript">
+    Docbase.run({
+      method: 'github',
+      map: {        // mapping file location relative to the project directory
+          file: 'map.json',
+          path: ''
+      },
+      github: {     // github option has to be present if method is github
+          user: 'appbaseio',
+          repo: 'Docs',
+          path: 'src',
+          branch: 'master',
+          editGithubBtn: true
+      },
+      file: {
+          path: 'docs'
+      },
+      generic: {
+          baseurl: "https://raw.githubusercontent.com/appbaseio/Docs/master",
+          path: "src"
+      },
+      indexHtml: 'entry.html',
+      flatdocHtml: '/bower_components/docbase/html/flatdoc.html',
+      html5mode: false
+    });
+  </script>
+```
+
+where,  
+* ``method`` indicates the source of markdown files and can be one of "github", "file" or "generic".
+* ``map`` is the mapping file location - A map is like a sitemap, showing the directory structure layout and allows adding pretty labels to folder and file names.
+* ``github`` is a top level field that is relevant when the selected method is ``github``. It has obvious parameters such as ``user``, ``repo``, ``branch`` and ``editGithubBtn``.
+* ``file`` similarly is a top level field that is relevant when the selected method is ``file``. It only has one parameter ``path``, a relative / absolute location from the root directory where the docs are hosted.
+* ``generic`` is a top level field that is relevant when the selected method is ``generic``. This can take markdown files hosted anywhere - github, bitbucket, dropbox, your own servers. It only needs a ``baseurl`` and a relative path under which the docs are kept.
+
+### Step 2: Configuring map.json
+
+map.json is an optional file (if you host docs on github) that allows specifying the docs directory structure and adding pretty labels, specified under the ``map`` field in the index.html configs.
+
+It looks something like this:
+
+```js
+map.json
+
+{ 
+  "version_name": [
       {
         "name": "folder1_name",
-        "label": "folder1_label"
+        "label": "folder1_label",
+        "files": [
+            {
+                "name": "innerFile1_name",
+                "label": "innerFile1_label"
+            }
+        ]
       }, {
-        "name": "folder2_name",
-        "label": "folder2_label"
-      }],
-      "other_version_name": [ ... ]
-    }
+        "name": "rootFile1_name",
+        "label": "rootFile1_label"
+      }
+  ],
+  "other_version_name": [ ... 
+  ]
+}
+```
+
+where,  
+``version_name`` is the top level field indicating the mapping for a particular documentation version, like v1.0.  
+A version can contain folders and files. ``name`` is the actual 
 
 ## Contributing
 
--- bower_components/docbase/scripts/docbase.js
+++ bower_components/docbase/scripts/docbase.js
@@ -80,8 +80,8 @@
 
         angApp = angular
             .module(options.angularAppName, ['ngRoute'])
-            .factory('FlatdocService', ['$q', '$route', '$location', '$anchorScroll', Route.fetch])
-            .controller('URLCtrl', ['$scope', '$location', 'data', Route.URLCtrl])
+            .factory('FlatdocService', ['$q', '$route', '$location', '$anchorScroll', '$http', Route.fetch])
+            .controller('URLCtrl', ['$scope', '$location','$filter', 'data', 'commits', Route.URLCtrl])
             .controller('MainCtrl', ['$scope', '$location', '$timeout', Route.mainCtrl])
             .config(['$routeProvider', '$locationProvider', Route.config])
             .run(
@@ -197,6 +197,9 @@
                 return FlatdocService.getData().then(function(data) {
                     return data;
                 });
+            },
+            commits:function(FlatdocService){
+                return FlatdocService.getCommits();
             }
         };
 
@@ -271,7 +274,7 @@
         });
     };
 
-    Route.fetch = function($q, $route, $location, $anchorScroll) {
+    Route.fetch = function($q, $route, $location, $anchorScroll, $http) {
         function fetcher() {
             var deferred = $q.defer();
             var options = Docbase.options;
@@ -308,6 +311,7 @@
                     retObj.github = url;
 
                     Events.parsed = false;
+                    console.log(fileURL);
                     Flatdoc.file(fileURL)(function(err, markdown) {
                         markdown = markdown.split('\n');
                         var obj = markdown.shift();
@@ -348,7 +352,8 @@
                         deferred.resolve(retObj);
                     });
 
-                } else {
+                } 
+                else {
                     retObj.github = false;
                     deferred.resolve(retObj);
                 }
@@ -359,13 +364,18 @@
         return {
             getData: function() {
                 return new fetcher();
+            },
+            getCommits: function(){
+                var options = Docbase.options;
+                var file_path = $route.current.params;
+                var full_path = options.github.path+'/'+file_path.version+'/'+file_path.folder+'/'+file_path.file;
+                return $http.get('https://api.github.com/repos/'+ options.github.user +'/'+ options.github.repo+'/commits?path='+full_path+'.md');
             }
         };
     };
 
-    Route.URLCtrl = function($scope, $location, data) {
+    Route.URLCtrl = function($scope, $location,$filter, data, commits) {
         $location.path(data.locationPath);
-
         if (!data.fail) {
             $scope.versions = data.versions;
             $scope.currentVersion = data.currentVersion;
@@ -373,12 +383,47 @@
             $scope.github = data.github;
 
             var content = data.markdown;
-
+            var contribut_array = [];
             $('[role="flatdoc-content"]').html(content.content.find('>*'));
             $('[role="flatdoc-menu"]').html(Flatdoc.menuView(content.menu));
 
             jWindow.trigger('flatdoc:ready');
         }
+
+        var extra_container = $("<div>").addClass('extra_container');
+        if(commits.status == 200){
+            var commits_data = commits.data;
+            var commiter_data = $filter('date')(commits.data[0].commit.committer.date, 'mediumDate');
+            var last_date = $('<span>').addClass('pull-right modified-date').html('Last Modified On : <a href="'+commits.data[0].html_url+'">'+commiter_data+'</a>');
+        
+            var contributors_data = commits_data;
+            var contributors = $('<div>').addClass('contributor-container');
+            for(var i =0; i < contributors_data.length; i++ ){
+                var contributor_d = contributors_data[i].committer;
+                if(jQuery.inArray(contributor_d.login, contribut_array) == -1)
+                {
+                    contribut_array.push(contributor_d.login);
+                    var contributor_img = $('<img>').addClass('contributor_img img-rounded').attr({
+                        'src':contributor_d.avatar_url,
+                        'alt':contributor_d.login
+                    });
+                    var contributor = $('<a>').addClass('contributor').attr({
+                        'href':contributor_d.html_url,
+                        'title':contributor_d.login,
+                        'target':'_blank'
+                    }).append(contributor_img);
+                    contributors.append(contributor);            
+                }
+            }
+            var contributors_header = $('<div>').addClass('contributors_header').append('Contributors').append(last_date);
+            $(extra_container).prepend(contributors).prepend(contributors_header);
+        
+
+        }
+            
+        var div2 = $('<div>').addClass('clearFix');
+        $('[role="flatdoc-content"]').prepend(div2).prepend(extra_container);
+
     };
 
     Route.mainCtrl = function($scope, $location, $timeout) {
