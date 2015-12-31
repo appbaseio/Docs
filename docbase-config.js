598ef30c6e1637f115e030774d0b34fb1e0c81a5
Siddharth Kothari <sids.aquarius@gmail.com>
2015-12-31 15:30:15 +0530

    fix comma typo

--- docbase-config.js
+++ docbase-config.js
@@ -74,7 +74,7 @@ var docbaseConfig = {
                       "name": "intro"
                   }
               ]
-          }
+          },
           {
               "label": "FAQs",
               "name": "FAQs",
