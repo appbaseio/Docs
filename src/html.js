0bc70de64df26b56bf8b4f9371de3adb01be849c
Divyanshu Maithani <div.blackcat@gmail.com>
2018-07-17 13:44:47 +0530

    Add hotjar script

--- src/html.js
+++ src/html.js
@@ -36,6 +36,20 @@ export default class HTML extends Component {
           />
           <link rel="icon" href="/favicon.ico" />
           {this.props.headComponents}
+          <script
+						dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:22389,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+							`,
+            }}
+          />
           {js}
           {css}
         </head>
