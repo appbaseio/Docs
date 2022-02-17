9a8d1eaf48d5a560790907e357af353b5c05ba68
Siddharth Kothari <sids.aquarius@gmail.com>
2022-02-17 16:35:25 +0530

    format(building-search-ui): add interactive quiz link for choosing a library

--- src/pages/docs/reactivesearch/ui-customization.js
+++ src/pages/docs/reactivesearch/ui-customization.js
@@ -14,6 +14,10 @@ const Customization = props => {
 			<h2>UI Customization Guide</h2>
 			<p>In this guide, we will discuss the customization possibilities with the ReactiveSearch UI libraries.</p>
 			<img src="https://i.imgur.com/W3Fc4CB.jpg" alt="UI customization guide" max-width="800px"></img>
+            <div style={{marginTop: '20px'}}>
+                <p style={{marginRight: '10px', display: 'inline'}}>Take this 2 mins interactive quiz to get a UI library recommendation based on your use-case</p>
+                <button data-tf-popup="o6CkRn05" data-tf-iframe-props="title=Choose UI Library" data-tf-medium="snippet" style={{all: 'unset', fontFamily: 'Helvetica,Arial,sans-serif', display: 'inline-block', maxWidth: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', backgroundColor: '#0445AF', color: '#FFFFFF', fontSize: '16px', borderRadius: '20px', padding: '0 26px', fontWeight: 'bold', height: '40px', cursor: 'pointer', lineHeight: '40px', textAlign: 'center', margin: 0, textDecoration: 'none'}}>Take interactive quiz 🪄</button>
+            </div>
 			<div className="container">
 				<h3>Theming</h3>
 				<p>
