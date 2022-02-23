2c87e2483b55d35bb516a306431a6fbee05f627e
lohi212 <suvvarilohita@gmail.com>
2022-02-24 00:13:43 +0530

    fix(pwa): maskable icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,19 @@ const plugins = [
 			theme_color: `#343f44`,
 			display: `minimal-ui`,
 			icon: `static/favicon.png`,
+			icons: [
+				{
+				  src: `static/favicon.pngg`,
+				  sizes: `192x192`,
+				  type: `image/png`,
+				  purpose: "any maskable"
+				},
+				{
+				  src: `static/favicon.png`,
+				  sizes: `512x512`,
+				  type: `image/png`,
+				},
+			],
 		},
 	},
 	`gatsby-plugin-offline`,
