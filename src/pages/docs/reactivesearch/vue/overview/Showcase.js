87ff4776f45bb2d4cfb0c6251113a72e6da0f94d
Siddharth Kothari <sids.aquarius@gmail.com>
2022-03-09 21:30:48 +0530

    fix(showcase): fixes ReactiveSearch React and Vue showcase demos (#286)
    * fix(navbar): remove broken FAQs link
    
    * fix(react showcase): fixes ReactiveSearch React showcase demo
    
    * fix(vue showcase): fixes ReactiveSearch Vue showcase demo

--- src/pages/docs/reactivesearch/vue/overview/Showcase.js
+++ src/pages/docs/reactivesearch/vue/overview/Showcase.js
@@ -15,8 +15,11 @@ import PostLayout from '../../../../../components/PostLayout';
 import ShowcaseComponent from '../../../../../components/ShowcaseComponent';
 
 const settings = {
-	app: 'airbeds-yj',
-	credentials: 'eRPhaUPwK:22f9279e-3b86-432d-a9b0-ba8a6f07f459',
+	app: 'clone-airbeds',
+	credentials: '3f7ed293fbe0:47ff004a-4722-49fe-bc48-0ff0286e4de0',
+    url: 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io',
+    enableAppbase: true,
+    mapKey: 'AIzaSyA9JzjtHeXg_C_hh_GdTBdLxREWdj3nsOU',
 	theme: {
 		colors: {
 			textColor: '#738a94',
@@ -37,34 +40,54 @@ const dataSearchProps = {
 const multilistProps = {
 	dataField: 'accommodates',
 	componentId: 'multilist',
-	react: { and: ['search'] },
+	react: { and: ['search', 'togglebutton',
+    'ratingfilter',
+    'rangeslider',
+    'search',
+    'singledropdownlist',
+    'singlelist'] },
 	className: 'showcase-list',
 	showSearch: false,
 	placeholder: 'Select Accommodates',
 };
 
 const singleListProps = {
-	dataField: 'property_type.raw',
+	dataField: 'property_type.keyword',
 	componentId: 'singlelist',
-	react: { and: ['search'] },
+	react: { and: ['search', 'togglebutton',
+    'ratingfilter',
+    'rangeslider',
+    'search',
+    'singledropdownlist',
+    'multilist'] },
 	className: 'showcase-list',
 	showSearch: false,
 	placeholder: 'Select Property Type',
 };
 
 const singleDropdownProps = {
-	dataField: 'property_type.raw',
+	dataField: 'property_type.keyword',
 	componentId: 'singledropdownlist',
 	className: 'dropdown-list',
-	react: { and: ['search'] },
+	react: { and: ['search', 'togglebutton',
+    'ratingfilter',
+    'rangeslider',
+    'search',
+    'singelist',
+    'multilist'] },
 	showSearch: false,
 };
 
 const rangeSliderProps = {
 	dataField: 'price',
 	componentId: 'rangeslider',
-	react: { and: ['search'] },
-	showHistogram: false,
+	react: { and: ['search', 'togglebutton',
+    'ratingfilter',
+    'multilist',
+    'search',
+    'singledropdownlist',
+    'singlelist'] },
+	showHistogram: true,
 	tooltipTrigger: 'hover',
 	rangeLabels: (min, max) => ({
 		start: `$${min}`,
@@ -72,9 +95,63 @@ const rangeSliderProps = {
 	}),
 };
 
+const dateRangeProps = {
+	componentId: 'date',
+	dataField: 'date_from',
+	numberOfMonths: 2,
+	customQuery: value => {
+		let query = null;
+		if (value) {
+			query = [
+				{
+					range: {
+						date_from: {
+							gte: moment(value.start).format('YYYYMMDD'),
+						},
+					},
+				},
+				{
+					range: {
+						date_to: {
+							lte: moment(value.end).format('YYYYMMDD'),
+						},
+					},
+				},
+			];
+		}
+		return query ? { query: { bool: { must: query } } } : null;
+	},
+	initialMonth: new Date('04/01/2017'),
+};
+
+const ratingsFilterProps = {
+	componentId: 'ratingfilter',
+	dataField: 'beds',
+	dimmedIcon: (
+		<img
+			style={{ width: 22, height: 22, marginLeft: 5 }}
+			alt="Inactive Bed"
+			src="https://img.icons8.com/pastel-glyph/2x/bed--v2.png"
+		/>
+	),
+	icon: (
+		<img
+			style={{ width: 22, height: 22, marginLeft: 5 }}
+			alt="Active Bed"
+			src="https://img.icons8.com/cotton/2x/bed--v2.png"
+		/>
+	),
+	data: [
+		{ start: 4, end: 5, label: '4 & up' },
+		{ start: 3, end: 5, label: '3 & up' },
+		{ start: 2, end: 5, label: '2 & up' },
+		{ start: 1, end: 5, label: '> 1 vote' },
+	],
+};
+
 const toggleButtonProps = {
 	componentId: 'togglebutton',
-	dataField: 'room_type.raw',
+	dataField: 'room_type.keyword',
 	data: [
 		{ label: 'Entire home/apt', value: 'Entire home/apt' },
 		{ label: 'Private Room', value: 'Private room' },
