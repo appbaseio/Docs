56f8b97ee103652ad7924d83f811513af573b2c0
lohi212 <suvvarilohita@gmail.com>
2022-03-15 00:17:19 +0530

    feat: remove unused

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,32 +9,3 @@ exports.createPages = ({ graphql, actions }) =>
 
 exports.onCreateNode = async ({ node, getNode, actions }) =>
 	await onCreateNode.createMarkdownNodeFields({ node, getNode, actions });
-
-
-exports .onCreateWebpackConfig = ({
-	stage,
-	rules,
-	loaders,
-	plugins,
-	actions,
-  }) => {
-	  actions.setWebpackConfig({
-		optimization: {
-			splitChunks: {
-				name: false,
-				chunks: `all`,
-				cacheGroups: {
-					default: false,
-					vendors: false,
-					commons: {
-					  name: `vendors`,
-					  chunks: `all`,
-					  // if a chunk is used more than half the components count,
-					  // we can assume it's pretty global
-					  minChunks: 2,
-					},
-				}
-			}
-		}
-	  })
-  }
