29838a9407211c20057da53073b1b6ebdaa78565
lohi212 <suvvarilohita@gmail.com>
2022-03-21 18:43:02 +0530

    format doc

--- src/components/common/ReactivesearchLogo.js
+++ src/components/common/ReactivesearchLogo.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Logo = ({ theme }) => {
-    return (
-        <img 
-            src={`${theme === 'dark' ? 'https://cdn.jsdelivr.net/gh/appbaseio/cdn@dev/appbase/logos/reactivesearch-white.svg' : 'https://cdn.jsdelivr.net/gh/appbaseio/cdn@dev/appbase/logos/reactivesearch-black.svg'}`}
-        />
-    )
-}
+	return (
+		<img
+			src={`${
+				theme === 'dark'
+					? 'https://cdn.jsdelivr.net/gh/appbaseio/cdn@dev/appbase/logos/reactivesearch-white.svg'
+					: 'https://cdn.jsdelivr.net/gh/appbaseio/cdn@dev/appbase/logos/reactivesearch-black.svg'
+			}`}
+		/>
+	);
+};
 
 export default Logo;
