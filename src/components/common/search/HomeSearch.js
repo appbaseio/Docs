fc4dba448adf54ab2131ead496697abe1d4ee207
lohi212 <suvvarilohita@gmail.com>
2022-03-24 19:17:09 +0530

    fix: fixed footer in suggestions container

--- src/components/common/search/HomeSearch.js
+++ src/components/common/search/HomeSearch.js
@@ -167,15 +167,17 @@ const HitTemplate = ({ hit, currentValue }) => {
 						dangerouslySetInnerHTML={{ __html: highlightedToken }}
 					/>
 				</div>
-				<svg
-					className="icon-position"
-					xmlns="http://www.w3.org/2000/svg"
-					alt="Recent Search"
-					viewBox="0 0 24 24"
-					>
-					<path d="M0 0h24v24H0z" fill="none" />
-					<path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z" />
-				</svg>
+				{!currentValue ? (
+					<svg
+						className="icon-position"
+						xmlns="http://www.w3.org/2000/svg"
+						alt="Recent Search"
+						viewBox="0 0 24 24"
+						>
+						<path d="M0 0h24v24H0z" fill="none" />
+						<path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z" />
+					</svg>
+				) : null }
 			</div>
 		</Link>
 	);
@@ -253,14 +255,15 @@ class AutoComplete extends React.Component {
 
 	renderSuggestionsContainer = ({ containerProps, children, query }) => {
 		const { value } = this.state;
-		console.log(children, children?.props?.items);
 		return (
-			<div {...containerProps}>
+			<div {...containerProps} >
 				{(children && children?.props?.items) ? (
 					<div>
-						{children.props?.items.map((item) => (
-							<HitTemplate hit={item} currentValue={value} />
-						))}
+						<div className='autosuggest-content'>
+							{children.props?.items.map((item) => (
+								<HitTemplate hit={item} currentValue={value} />
+							))}
+						</div>
 						<div className='autosuggest-footer-container'>
 							{/* color: '#8792a2', background: '#f7fafc' */}
 							<div>↑↓ Navigate</div>
