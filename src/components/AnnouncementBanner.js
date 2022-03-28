d2867902e1339c806a4bf28e944fea3ae8b0b513
lohi212 <suvvarilohita@gmail.com>
2022-03-28 16:51:10 +0530

    fix: announcement banner styles

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -1,28 +1,6 @@
 import React, { useState } from 'react';
 import { css } from 'emotion';
 
-const bannerStyles = css`
-	.announcement-banner {
-		background: #23263b;
-		padding: 12px;
-		color: #fff;
-		display: flex;
-		justify-content: center;
-		font-size: 16px;
-		.link-container {
-			color: #fff;
-			text-decoration: underline;
-			&: hover {
-				color: #1890ff;
-			}
-		}
-		.close-icon {
-			position: absolute;
-			right: 30px;
-			cursor: pointer;
-		}
-	}
-`;
 const AnnouncementBanner = () => {
 	const [showBanner, setShowBanner] = useState(
 		typeof window !== 'undefined' ? localStorage.getItem('announcementBanner') === 'true' : false,
@@ -33,25 +11,25 @@ const AnnouncementBanner = () => {
 	}
 
 	return (
-		<div css={bannerStyles}>
+		<div>
 			{showBanner ? (
 				<div className="announcement-banner">
 					<a
-						className="link-container"
+						className="banner-link-container"
 						href="https://www.appbase.io/"
 					>
 						appbase.io
 					</a>
 					&nbsp; is now &nbsp;
 					<a
-						className="link-container"
+						className="banner-link-container"
 						href="https://www.reactivesearch.io/"
 					>
 						reactivesearch.io
 					</a>{' '}
-					<a href="#">&nbsp;[Read the announcement]</a>
+					<a href="#" style={{ color: ' #1890ff'}}>&nbsp;[Read the announcement]</a>
 					<img
-						src="/static/images/close.svg"
+						src="/images/close.svg"
 						width={20}
 						alt="close-icon"
 						className="close-icon"
