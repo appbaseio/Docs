79581716014942dfc6f271c308d98e555c6f69be
Siddharth Kothari <sids.aquarius@gmail.com>
2022-03-29 20:40:30 +0530

    fix: line height for banner

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -14,20 +14,8 @@ const AnnouncementBanner = () => {
 		<div>
 			{showBanner ? (
 				<div className="announcement-banner">
-					<a
-						className="banner-link-container"
-						href="https://www.appbase.io/"
-					>
-						appbase.io
-					</a>
-					&nbsp; is now &nbsp;
-					<a
-						className="banner-link-container"
-						href="https://www.reactivesearch.io/"
-					>
-						reactivesearch.io
-					</a>{' '}
-					<a href="#" style={{ color: ' #1890ff'}}>&nbsp;[Read the announcement]</a>
+					appbase.io is now reactivesearch.io{' '}
+					<a href="#" style={{ color: ' #1890ff'}}>&nbsp;Read the announcement</a>
 					<img
 						src="/images/close.svg"
 						width={20}
