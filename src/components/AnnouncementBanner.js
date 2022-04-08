bb4c7b72422380cfa06afd25c8db2ec23f03ce28
lohi212 <suvvarilohita@gmail.com>
2022-04-08 15:46:01 +0530

    feat: added banner announcement link

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { css } from 'emotion';
 
-const AnnouncementBanner = ({showBanner, setShowBanner}) => {
-		
+const AnnouncementBanner = ({ showBanner, setShowBanner }) => {
 	return (
 		<div>
 			{showBanner ? (
 				<div className="announcement-banner">
 					appbase.io is now reactivesearch.io{' '}
-					<a href="#" style={{ color: ' #1890ff'}}>&nbsp;Read the announcement</a>
+					<a
+						href="https://blog.reactivesearch.io/appbase-io-announcement"
+						target="_blank"
+						style={{ color: ' #1890ff' }}
+					>
+						&nbsp;Read the announcement
+					</a>
 					<img
 						src="/images/close.svg"
 						width={20}
