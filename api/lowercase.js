963655347c097a7b0bc7063f7a66fc2099b1ae81
Siddharth Kothari <sids.aquarius@gmail.com>
2023-07-12 17:59:13 +0700

    fix(routing): update redirection code to 301 and reset 404 page

--- api/lowercase.js
+++ api/lowercase.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const lowercaseUrl = req.url.toLowerCase();
-  res.writeHead(308, { Location: lowercaseUrl });
-  res.end();
+	const lowercaseUrl = req.url.toLowerCase();
+	res.writeHead(301, { Location: lowercaseUrl });
+	res.end();
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useEffect } from 'react';
+import { Link, navigate } from 'gatsby';
 
 import { Spirit } from '../styles/spirit-styles';
 import { Layout } from '../components/common/layout';
 import { Icon } from '../components/common';
 
-const NotFoundPage = () => (
-	<Layout headerDividerStyle="shadow">
-		<div className={`${Spirit.page.m} pt-vw5 pb-vw5 flex flex-column items-center`}>
-			<Icon name="four-o-four-icon" className="w15 h-auto stroke-lightgrey" />
-			<h1 className={Spirit.h1}>404</h1>
-			<p className={`${Spirit.p} midgrey`}>You just hit a route that doesn&#39;t exist.</p>
-			<div className="flex mt5">
-				<Link to="/" className="pa2 dib blue hover-darkgrey link br b--whitegrey">
-					Docs
-				</Link>
-				<a href="https://reactivesearch.io" className="pa2 dib blue hover-darkgrey link">
-					Reactivesearch.io
-				</a>
+const NotFoundPage = () => {
+	useEffect(() => {
+		navigate('/');
+	}, []);
+
+	return (
+		<Layout headerDividerStyle="shadow">
+			<div className={`${Spirit.page.m} pt-vw5 pb-vw5 flex flex-column items-center`}>
+				<Icon name="four-o-four-icon" className="w15 h-auto stroke-lightgrey" />
+				<h1 className={Spirit.h1}>404</h1>
+				<p className={`${Spirit.p} midgrey`}>
+					You just hit a route that doesn&#39;t exist.
+				</p>
+				<div className="flex mt5">
+					<Link to="/" className="pa2 dib blue hover-darkgrey link br b--whitegrey">
+						Docs
+					</Link>
+					<a
+						href="https://reactivesearch.io"
+						className="pa2 dib blue hover-darkgrey link"
+					>
+						Reactivesearch.io
+					</a>
+				</div>
 			</div>
-		</div>
-	</Layout>
-);
+		</Layout>
+	);
+};
 
 export default NotFoundPage;
