faabfb73a00fa96eea1fe8d9e2b6e29796f42e64
Siddharth Kothari <sids.aquarius@gmail.com>
2023-07-12 16:50:56 +0700

    format(docs): handle all lowercase routes

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -41,16 +41,27 @@ module.exports.createMarkdownPages = async ({ graphql, actions }) => {
 
 					// Convert URLs accessed at "/reactivesearch/vue/SearchBox" -> "/reactivesearch/vue/searchbox"
 					if (
+						node &&
+						node.fields &&
 						node.fields.slug &&
-						node.fields.slug.includes('/docs/reactivesearch/vue/') &&
-						!node.fields.slug.includes('/docs/reactivesearch/vue/v1')
+						node.fields.slug.toLowerCase().startsWith('/docs/reactivesearch/v4')
 					) {
-						// Create a duplicate page for "/reactivesearch/vue/searchbox"
-						const optionsForDuplicatePage = {
+						// Create a page with /v4 and one with /react
+						createPage({
+							...options,
+							path: node.fields.slug.toLowerCase().replace('/v4', '/react'),
+						});
+						createPage(options);
+					} else if (
+						node.fields.slug &&
+						node.fields.slug.toLowerCase().startsWith('/docs/reactivesearch')
+					) {
+						// Create a lower case route for "/docs/reactivesearch/", i.e. Search UI pages
+						const optionsForLowerCaseRoute = {
 							...options,
 							path: node.fields.slug.toLowerCase(),
 						};
-						createPage(optionsForDuplicatePage);
+						createPage(optionsForLowerCaseRoute);
 						createPage(options);
 					} else {
 						createPage(options);
