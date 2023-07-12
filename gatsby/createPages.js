451d13b644be61fdd037c924b5266dd043d29277
Siddharth Kothari <sids.aquarius@gmail.com>
2023-07-12 17:17:00 +0700

    fix(routing): GPT-4 fixes routing | compile error

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -1,4 +1,19 @@
-module.exports.createMarkdownPages = async ({ graphql, actions }) => {
+const path = require(`path`);
+const _ = require(`lodash`);
+const { allMarkdownPosts } = require(`../utils/node-queries`);
+
+const createRedirects = ({ actions }) => {
+	const { createRedirect } = actions;
+
+	createRedirect({
+		fromPath: `/concepts`,
+		isPermanent: true,
+		redirectInBrowser: true,
+		toPath: `/concepts/introduction/`,
+	});
+};
+
+const createMarkdownPages = async ({ graphql, actions }) => {
 	const { createPage, createRedirect } = actions;
 	const queryPromises = [];
 
@@ -9,7 +24,7 @@ module.exports.createMarkdownPages = async ({ graphql, actions }) => {
 					return reject(result.errors);
 				}
 
-				return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+				result.data.allMarkdownRemark.edges.forEach(({ node }) => {
 					const DocTemplate = path.resolve(`./src/templates/markdown/post.js`);
 					const lowerCaseSlug = node.fields.slug.toLowerCase();
 
@@ -17,20 +32,17 @@ module.exports.createMarkdownPages = async ({ graphql, actions }) => {
 						path: lowerCaseSlug,
 						component: DocTemplate,
 						context: {
-							// Data passed to context is available
-							// in page queries as GraphQL variables.
 							slug: node.fields.slug,
 							section: node.fields.section,
 						},
 					};
 
 					if (node.fields.slug !== lowerCaseSlug) {
-						// Create a redirect from the original (potentially mixed case) slug to the lower-case version
 						createRedirect({
 							fromPath: node.fields.slug,
 							toPath: lowerCaseSlug,
 							isPermanent: true,
-							force: true,  // "force: true" avoids conflict with existing paths
+							force: true,
 						});
 					}
 
@@ -40,28 +52,30 @@ module.exports.createMarkdownPages = async ({ graphql, actions }) => {
 						node.fields.slug &&
 						node.fields.slug.toLowerCase().startsWith('/docs/reactivesearch/v4')
 					) {
-						// Create a page with /v4 and one with /react
 						createPage({
 							...options,
 							path: lowerCaseSlug.replace('/v4', '/react'),
 						});
 
-						// Redirect from the /v4 version to the /react version
 						createRedirect({
 							fromPath: lowerCaseSlug,
 							toPath: lowerCaseSlug.replace('/v4', '/react'),
 							isPermanent: true,
 						});
 					} else {
-						// Create the page with the lower-case slug
 						createPage(options);
 					}
 
-					return resolve();
+					resolve();
 				});
 			});
 		}),
 	);
 
-	return Promise.all(queryPromises);
+	await Promise.all(queryPromises);
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+	await createRedirects({ actions });
+	await createMarkdownPages({ graphql, actions });
 };
