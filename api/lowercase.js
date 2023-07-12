b54547ccc59b6350f37edba1cd775194fb43d8db
Siddharth Kothari <sids.aquarius@gmail.com>
2023-07-12 18:05:47 +0700

    fix(routing): handle 404 redirection

--- api/lowercase.js
+++ api/lowercase.js
@@ -1,5 +1,13 @@
 module.exports = (req, res) => {
-	const lowercaseUrl = req.url.toLowerCase();
-	res.writeHead(301, { Location: lowercaseUrl });
+	const originalUrl = req.url;
+	const lowercaseUrl = originalUrl.toLowerCase();
+
+	if (originalUrl === lowercaseUrl) {
+		// If the original URL is already lowercase, redirect to the 404 page
+		res.writeHead(302, { Location: '/404' });
+	} else {
+		// Otherwise, redirect to the lowercase version of the URL
+		res.writeHead(301, { Location: lowercaseUrl });
+	}
 	res.end();
 };
